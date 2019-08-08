da0fe69c5906b66bd71794cfefb9376db5488006
Hawat <gmhawat@gmail.com>
2019-08-08 13:33:23 +0300

    Update controller

--- app/controllers/api/v0/packages_controller.rb
+++ app/controllers/api/v0/packages_controller.rb
@@ -1,9 +1,4 @@
 class Api::V0::PackagesController < ApplicationController
-  def index
-    @packages = Package.all
-    render json: @packages
-  end
-
   def show
     render json: Package.where("lower(package_name) like ?", "%#{params[:id]}%")
   end
