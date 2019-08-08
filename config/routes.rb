3975606889ccbb58cbb122b6865b06bc12de24c0
Hawat <gmhawat@gmail.com>
2019-08-08 13:31:20 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v0' do
       resources :packages
-      namespace 'search' do
-        resources :packages
-      end
     end
   end
 end
