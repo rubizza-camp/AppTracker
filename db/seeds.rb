8720bbd90d0b8df2fb70ee0eafdd3619ae8e8180
IVAN BARYSIUK <Jdjjad89coq5ven5uqoweOOvqw3n@gmail.com>
2019-08-09 10:22:55 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,77 +9,77 @@
 package = Package.create(
   name: 'Twitter',
   company_name: 'Rubizza',
-  review_count: 777,
+  review_count: 418122,
   average_rating: 4.9,
-  downloads_count: 111
+  downloads_count: 11159081
 )
 package = Package.create(
   name: 'Tetris',
   company_name: 'Rubizza',
-  review_count: 777,
-  average_rating: 4.9,
-  downloads_count: 111
+  review_count: 1519241,
+  average_rating: 4.2,
+  downloads_count: 12912932
 )
 package = Package.create(
   name: 'TabGame',
   company_name: 'Rubizza',
-  review_count: 777,
-  average_rating: 4.9,
-  downloads_count: 111
+  review_count: 91827,
+  average_rating: 3.9,
+  downloads_count: 1112412
 )
 package = Package.create(
   name: 'Testings',
   company_name: 'Rubizza',
-  review_count: 777,
-  average_rating: 4.9,
-  downloads_count: 111
+  review_count: 777341,
+  average_rating: 3.1,
+  downloads_count: 1215211
 )
 package = Package.create(
   name: 'Tabulars',
   company_name: 'Rubizza',
-  review_count: 777,
-  average_rating: 4.9,
-  downloads_count: 111
+  review_count: 91822,
+  average_rating: 3.9,
+  downloads_count: 912874
 )
 package = Package.create(
   name: 'Tuned',
   company_name: 'Rubizza',
-  review_count: 777,
-  average_rating: 4.9,
-  downloads_count: 111
+  review_count: 9127742,
+  average_rating: 3.8,
+  downloads_count: 81727
 )
 package = Package.create(
   name: 'Bootstrap',
   company_name: 'Rubizza',
-  review_count: 777,
-  average_rating: 4.9,
-  downloads_count: 111
+  review_count: 9213848,
+  average_rating: 5.0,
+  downloads_count: 666298992
 )
 package = Package.create(
   name: 'BoomID',
   company_name: 'Rubizza',
-  review_count: 777,
+  review_count: 124919,
   average_rating: 4.9,
-  downloads_count: 111
+  downloads_count: 91928492
 )
 package = Package.create(
   name: 'Botter',
   company_name: 'Rubizza',
-  review_count: 777,
-  average_rating: 4.9,
-  downloads_count: 111
+  review_count: 7778,
+  average_rating: 2.8,
+  downloads_count: 111319
 )
 package = Package.create(
   name: 'Rake',
   company_name: 'Rubizza',
-  review_count: 777,
-  average_rating: 4.9,
-  downloads_count: 111
+  review_count: 7731247,
+  average_rating: 1.9,
+  downloads_count: 1119182
 )
 package = Package.create(
   name: 'Rails',
   company_name: 'Rubizza',
-  review_count: 777,
-  average_rating: 4.9,
-  downloads_count: 111
+  review_count: 9128847,
+  average_rating: 3.9,
+  downloads_count: 91283841
 )
