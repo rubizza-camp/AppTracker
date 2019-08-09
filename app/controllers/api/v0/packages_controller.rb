d833347f33ea50a29bd863d789c6ec76fb4d6ba9
Hawatz <49044357+Hawatz@users.noreply.github.com>
2019-08-09 16:28:05 +0300

    Test model, seed and search (#7)
    * Add test model, seeds and search
    
    * Update routes
    
    * Update controller
    
    * Fix rubocop offenses
    
    * Update names
    
    * Update rubocop metrics and search
    
    * Update all seeds
    
    * Add fast_jsonapi serializer
    
    * Update seeds for new search view
    
    * Update migration
    
    * Update packages_controller
    
    * Update seeds and package controller

---
+++ app/controllers/api/v0/packages_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  module V0
+    class PackagesController < ApplicationController
+      def index
+        search = Package.where('name ilike ?', "%#{params[:name]}%")
+        render json: PackageSerializer.new(search)
+      end
+    end
+  end
+end
