02328ea935c0bc4c9dbc18b064e67be346bf52b5
Hawat <gmhawat@gmail.com>
2019-08-09 10:24:55 +0300

    Update all seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,82 +1,74 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-packages = Package.create(
+packagess = Package.create(
   name: 'Twitter',
   company_name: 'Rubizza',
   review_count: 777,
   average_rating: 4.9,
   downloads_count: 111
 )
-package = Package.create(
+packages = Package.create(
   name: 'Tetris',
   company_name: 'Rubizza',
   review_count: 777,
   average_rating: 4.9,
   downloads_count: 111
 )
-package = Package.create(
+packages = Package.create(
   name: 'TabGame',
   company_name: 'Rubizza',
   review_count: 777,
   average_rating: 4.9,
   downloads_count: 111
 )
-package = Package.create(
+packages = Package.create(
   name: 'Testings',
   company_name: 'Rubizza',
   review_count: 777,
   average_rating: 4.9,
   downloads_count: 111
 )
-package = Package.create(
+packages = Package.create(
   name: 'Tabulars',
   company_name: 'Rubizza',
   review_count: 777,
   average_rating: 4.9,
   downloads_count: 111
 )
-package = Package.create(
+packages = Package.create(
   name: 'Tuned',
   company_name: 'Rubizza',
   review_count: 777,
   average_rating: 4.9,
   downloads_count: 111
 )
-package = Package.create(
+packages = Package.create(
   name: 'Bootstrap',
   company_name: 'Rubizza',
   review_count: 777,
   average_rating: 4.9,
   downloads_count: 111
 )
-package = Package.create(
+packages = Package.create(
   name: 'BoomID',
   company_name: 'Rubizza',
   review_count: 777,
   average_rating: 4.9,
   downloads_count: 111
 )
-package = Package.create(
+packages = Package.create(
   name: 'Botter',
   company_name: 'Rubizza',
   review_count: 777,
   average_rating: 4.9,
   downloads_count: 111
 )
-package = Package.create(
+packages = Package.create(
   name: 'Rake',
   company_name: 'Rubizza',
   review_count: 777,
   average_rating: 4.9,
   downloads_count: 111
 )
-package = Package.create(
+packages = Package.create(
   name: 'Rails',
   company_name: 'Rubizza',
   review_count: 777,
