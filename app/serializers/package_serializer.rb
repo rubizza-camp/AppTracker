d49d515a4c56d845a173046fb2292d4bd2190f4c
Hawat <gmhawat@gmail.com>
2019-08-09 12:13:49 +0300

    Update seeds for new search view

--- app/serializers/package_serializer.rb
+++ app/serializers/package_serializer.rb
@@ -1,4 +1,4 @@
 class PackageSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :company_name, :review_count, :average_rating, :downloads_count
+  attributes :name, :company_name, :review_count, :average_rating, :downloads_count, :icon_link
 end
--- db/migrate/2190809090354_create_another_packages.rb
+++ db/migrate/2190809090354_create_another_packages.rb
@@ -1,12 +1,13 @@
-class CreatePackages < ActiveRecord::Migration[5.2]
+class CreateAnotherPackages < ActiveRecord::Migration[5.2]
   def change
+    drop_table :another_packages
     create_table :packages do |t|
       t.string :name
       t.string :company_name
       t.integer :review_count
       t.float :average_rating
       t.integer :downloads_count
-
+      t.string :icon_link
       t.timestamps
     end
   end
