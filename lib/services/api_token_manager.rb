f17a16417f7c844fb24b74559ccc45ea2b7036ff
IVAN BARYSIUK <Jdjjad89coq5ven5uqoweOOvqw3n@gmail.com>
2019-08-13 17:11:00 +0300

    Correcting api_token_manager logic

--- lib/services/api_token_manager.rb
+++ lib/services/api_token_manager.rb
@@ -3,7 +3,10 @@ class Services
     class << self
       def token_with_credits(minimal_credits)
         avaliable_token = ApiToken.where("credits >= #{minimal_credits}").order(:credits).first
+        return nil if avaliable_token.nil?
+
         avaliable_token.update(credits: (avaliable_token.credits - minimal_credits))
+        avaliable_token.token
       end
     end
   end
