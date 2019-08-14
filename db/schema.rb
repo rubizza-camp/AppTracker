e912959b2ede16380317b59fda5c337af1af485a
IVAN BARYSIUK <Jdjjad89coq5ven5uqoweOOvqw3n@gmail.com>
2019-08-14 09:52:25 +0300

    Merging name 'token' with 'value'

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_132400) do
+ActiveRecord::Schema.define(version: 2019_08_14_064811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "api_tokens", force: :cascade do |t|
-    t.string "token"
+    t.string "value"
     t.integer "credits", default: 100
   end
 
