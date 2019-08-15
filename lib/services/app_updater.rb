b41565842cdf046963cdc06f8d544b66e5723c63
kartowkaateist <kartowkaateist@gmail.com>
2019-08-15 18:04:42 +0300

    Update dates

--- lib/services/app_updater.rb
+++ lib/services/app_updater.rb
@@ -20,10 +20,12 @@ class AppUpdater < AppMothership
   def update_one_app(name)
     AppDynamicUpdater.update(name)
     AppRatingsUpdater.update(name)
+    Services::ApiDateManager.update(App.where(title: name).first.id)
   end
 
   def update_all_apps
     AppDynamicUpdater.update_all
     AppRatingsUpdater.update_all
+    Services::ApiDateManager.update_all
   end
 end
