2104c5dab79c7bfc5a7398abd5a2972257671560
IVAN BARYSIUK <Jdjjad89coq5ven5uqoweOOvqw3n@gmail.com>
2019-08-15 17:16:55 +0300

    Update api_date_manager.rb

--- lib/services/api_date_manager.rb
+++ lib/services/api_date_manager.rb
@@ -4,12 +4,20 @@ class Services
       def last_date(app_id)
         last_date = ApiAppLastCatchDate.where(app_id: app_id).order(:date).last
         unless last_date.nil?
-          last_date_time = last_date.date
-          last_date.update(date: (DateTime.now))
-          last_date_time.to_date
+          if last_date.date.nil?
+            (DateTime.now - 1.month).to_date
+          else
+            last_date.date.to_date
+          end
         else
-          ApiAppLastCatchDate.create(:date => DateTime.now, :app_id => app_id)
+          ApiAppLastCatchDate.create(:date => nil, :app_id => app_id)
           (DateTime.now - 1.month).to_date
+        end
+      end
+
+      def update_all
+        ApiAppLastCatchDate.all.each do |api_date|
+          api_date.update(date: (DateTime.now))
         end
       end
     end
