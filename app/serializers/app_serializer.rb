2888d92b7283c95c02875c2e77c67c93d3ecc74b
Hawat <gmhawat@gmail.com>
2019-08-15 18:20:08 +0300

    Fix rubocop offenses

--- app/serializers/app_serializer.rb
+++ app/serializers/app_serializer.rb
@@ -1,8 +1,8 @@
 class AppSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :apple_app_id, :android_app_id, :title, 
+  attributes :id, :apple_app_id, :android_app_id, :title,
              :apple_url, :android_url, :short_description,
-             :long_description,:icon_url, :content_rating,
+             :long_description, :icon_url, :content_rating,
              :price, :dev_name, :dev_email, :dev_website
   attributes :search_info do
     "#{object.title} #{object.apple_url} #{object.android_url} #{object.icon_url}"
@@ -10,4 +10,3 @@ class AppSerializer
   has_many :ratings
   has_many :dinamic_infos
 end
-      
