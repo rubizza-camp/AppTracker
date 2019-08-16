8cbefb601cb1d2223b6ced01e07c635389acab85
Hawat <gmhawat@gmail.com>
2019-08-16 10:16:16 +0300

    Fix rubocop offenses

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -3,12 +3,12 @@ module Api
     class AppsController < ApplicationController
       def index
         search = App.where('title ilike ?', "%#{params[:title]}%")
-        render json: search.to_json(only: [:title, :apple_url, :android_url, :icon_url])
+        render json: search.to_json(only: %i[title apple_url android_url icon_url])
       end
 
       def show
         app = App.find_by title: params[:title]
-        render json: app.to_json(include: [:ratings, :dynamic_infos])
+        render json: app.to_json(include: %i[ratings dynamic_infos])
       end
     end
   end
