3390966d2bcd501265ba3e963f58c9a9a4d22d82
Hawat <gmhawat@gmail.com>
2019-08-16 18:00:54 +0300

    Add serializer and search from start of string

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -2,13 +2,13 @@ module Api
   module V1
     class AppsController < ApplicationController
       def index
-        search = App.where('title ilike ?', "%#{params[:title]}%")
-        render json: search.to_json(only: %i[title apple_url android_url icon_url])
+        search = App.where('title like :prefix', prefix: "#{params[:title]}%")
+        render json: AppSerializer.new(search, fields: { app: [:search_data] })
       end
 
       def show
         app = App.find_by title: params[:title]
-        render json: app.to_json(include: %i[ratings dynamic_infos])
+        render json: AppSerializer.new(app)
       end
     end
   end
---
+++ app/serializers/app_serializer.rb
@@ -0,0 +1,14 @@
+class AppSerializer
+  include FastJsonapi::ObjectSerializer
+  #  has_many :ratings
+  #  has_many :dynamic_infos
+
+  attributes :apple_app_id, :android_app_id, :title,
+             :apple_url, :android_url, :short_description,
+             :long_description, :icon_url, :content_rating,
+             :price, :dev_name, :dev_email, :dev_website
+
+  attribute :search_data do |app|
+    { title: :"#{app.title}", apple: :"#{app.apple_url}", android: :"#{app.android_url}", icon: :"#{app.icon_url}" }
+  end
+end
