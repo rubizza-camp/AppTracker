9fe2d80688f2a64b1b831e98e43fc96da7b2db53
Hawat <gmhawat@gmail.com>
2019-08-17 03:55:46 +0300

    Rename search to apps from apps controller

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -2,8 +2,8 @@ module Api
   module V1
     class AppsController < ApplicationController
       def index
-        search = App.where('title like :prefix', prefix: "#{params[:title]}%")
-        render json: AppSerializer.new(search, fields: { app: [:search_data] })
+        apps = App.where('title like :prefix', prefix: "#{params[:title]}%")
+        render json: AppSerializer.new(apps, fields: { app: [:search_data] })
       end
 
       def show
