0b02cfb5bc2d95a85981b52a9b73c86ea89e499b
kartowkaateist <kartowkaateist@gmail.com>
2019-08-22 10:55:22 +0300

    Add time zone

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module ApptrackerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.time_zone = 'Minsk'
   end
 end
