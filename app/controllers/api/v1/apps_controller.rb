5e52450d72e6f77db267cca700f95166a37e5dca
Hawat <gmhawat@gmail.com>
2019-08-22 16:53:10 +0300

    init

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class AppsController < ApplicationController
       def index
-        apps = App.where('title like :prefix', prefix: "#{params[:title]}%")
+        apps = App.where('title like :prefix', prefix: "#{params[:title].downcase}%")
         render json: AppIndexSerializer.new(apps)
       end
 
