0bb05c06a46ccd55ca6b35b0370e3705176edd1d
Hawat <gmhawat@gmail.com>
2019-08-25 14:04:29 +0300

    Add subs cotroller

--- app/controllers/api/v1/subscribers_controller.rb
+++ app/controllers/api/v1/subscribers_controller.rb
@@ -1,6 +1,21 @@
 module Api
   module V1
     class SubscribersController < ApplicationController
+      def create
+        app = App.find_by(title: app_params)
+        subscriber = Subscriber.find_or_create_by(sub_params)
+        subscription = Subscription.create([subscriber_id: subscriber.id, app_id: app.id])
+      end
+
+      private
+
+      def sub_params
+        params.permit(:email)
+      end
+
+      def app_params
+        params.permit(:title)
+      end
     end
   end
 end
