8298e7b0ac358073c2e1860e42e0389d168d17b0
Hawat <gmhawat@gmail.com>
2019-08-25 12:10:47 +0300

    Run annotate

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: subscriptions
+#
+#  id            :bigint           not null, primary key
+#  subscriber_id :bigint
+#  app_id        :bigint
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Subscription < ApplicationRecord
   belongs_to :app
   belongs_to :subscriber
