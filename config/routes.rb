19ae22e5ad4267ebfef1d1606ede9bd64d2f2258
Hawat <gmhawat@gmail.com>
2019-08-26 10:17:06 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace 'v1' do
-      resources :apps, param: :title, only: %i[index show]
-      resources :subscribers
+      resources :apps, param: :title, only: %i[index show] do
+        resources :subscribers
+      end
     end
   end
 end
