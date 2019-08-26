2da200dda132ff948c18d4928ce9cd1dbdc432e3
Hawat <gmhawat@gmail.com>
2019-08-26 11:31:58 +0300

    Update routes to users

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :apps, only: %i[index show] do
-        resources :subscribers, only: [:create]
+        resources :users, only: [:create]
       end
     end
   end
