4ac227ac4d2b0b01f99e86edfd7ad2c77e8973cf
Sufl0wer <fr08ty@gmail.com>
2019-08-26 11:17:30 +0300

    style improvements

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -3,14 +3,14 @@ require './app/controllers/api/v1/apps_controller'
 
 describe "main page route" do
   it "routes to index" do
-    { :get => "/api/v1/apps" }.
-        should route_to(controller: "api/v1/apps", action: "index")
+    expect( get: "/api/v1/apps" ).
+        to route_to(controller: "api/v1/apps", action: "index")
   end
 end
 
 describe "app route" do
   it "routes to app page" do
-    { :get => "/api/v1/apps/title" }.
-        should route_to(controller: "api/v1/apps", action: "show", title: "title")
+    expect( get: "/api/v1/apps/title" ).
+        to route_to(controller: "api/v1/apps", action: "show", title: "title")
   end
 end
