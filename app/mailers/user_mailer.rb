54273787dee378a4c2954115c52285ae141765d8
Sufl0wer <fr08ty@gmail.com>
2019-08-27 13:16:11 +0300

    add missing files

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def registration_confirmation(user)
+    @user = user
+    mail(to: @user.email, subject: 'Email verification at AppTracker.club')
+  end
+
+  def notification_email(user)
+    @user = user
+    attachments.inline['meh.png'] = File.read("#{Rails.root}/app/views/user_mailer/meh.png")
+    mail(to: @user.email, subject: 'Major changes in tracked apps')
+  end
+end
