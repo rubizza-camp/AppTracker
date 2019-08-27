ea155d760dd655bc503aba0e7d5abf7a5f00b656
Sufl0wer <fr08ty@gmail.com>
2019-08-27 13:04:43 +0300

    add email views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'example.com',
+      user_name:             ENV["APPTRACKER_EMAIL"],#'AppTracker.realOne@gmail.com'
+      password:              ENV["APPTRACKER_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true }
 end
