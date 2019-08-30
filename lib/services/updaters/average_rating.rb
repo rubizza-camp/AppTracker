fae38df6399b7aeb6c27433dbb57ae57bc8d9793
kartowkaateist <kartowkaateist@gmail.com>
2019-08-30 17:22:00 +0300

    FINAL final fix

--- lib/services/updaters/average_rating.rb
+++ lib/services/updaters/average_rating.rb
@@ -1,32 +1,18 @@
 module Services
   module Updaters
     class AverageRating < Services::Base
-      attribute :total_rating
-      attribute :new_ratings
       attribute :average_rating
       attribute :current_app
 
       private
 
       def perform
-        @total_rating = 0.0
-        new_ratings
-        new_ratings.each do |local_rating|
-          @total_rating += local_rating.average_rating
-        end
+        @average_rating =
+          Rating.where(app_id: current_app.id, date: Time.zone.yesterday).average(:average_rating).round
         update_average
       end
 
-      def new_ratings
-        @new_ratings = Rating.where(app_id: current_app.id, date: Time.zone.yesterday)
-      end
-
-      def average_rating
-        (@average_rating = total_rating / new_ratings.count).to_i
-      end
-
       def update_average
-        average_rating
         current_app.update(rating: average_rating)
       end
     end
