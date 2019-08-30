dcea6c1ba2c4bb8c30013f289e58f276986714a7
KartowkaAteist <52112294+KartowkaAteist@users.noreply.github.com>
2019-08-30 16:11:40 +0300

    Cron jobs (#25)
    * It work?!
    
    * Gemfile
    
    * It work?!
    
    * Gemfile
    
    * qwe
    
    * I WANNA DIE
    
    * Final version
    
    * Fix bug with app return and some other things
    
    * Delete comments

--- lib/services/updaters/metadata.rb
+++ lib/services/updaters/metadata.rb
@@ -7,7 +7,10 @@ module Services
 
       def perform
         android_id = Services::Parsers::Searches.call(response:
-          Services::ApptweakApi::Searches.call(term: term, shop_type: 'android'))
+        Services::ApptweakApi::Searches.call(term: term, shop_type: 'android'))
+        app = App.find_by(android_app_id: android_id)
+        return app if app
+
         apple_id = Services::Parsers::Searches.call(response:
           Services::ApptweakApi::Searches.call(term: term, shop_type: 'ios'))
         ids = { android_app_id: android_id, apple_app_id: apple_id }
